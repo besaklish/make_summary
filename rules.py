ce9dfb5925e222e8c05e2ced5512fea2e6df768f
besaklish <besaklish@gmail.com>
2019-03-23 00:03:56 +0900

    Add reduce_sentences

--- rules.py
+++ rules.py
@@ -7,8 +7,13 @@ def get_first_sentence(paragraph):
     If there is a quotation mark at the beginning of the paragraph,
     return the quotation part.
     """
+    inline_quotation_mark_re = re.compile(r'^.*?[\"\'「].+?[\"\'」].*?[.。]')
     quotation_mark_re = re.compile(r'^[\"\'「].+?[\"\'」]')
-    first_sentence_re = re.compile(r'^.+?。')
+    first_sentence_re = re.compile(r'^.+?[\.。]')
+
+    inline_quotation_mark_match = inline_quotation_mark_re.match(paragraph)
+    if inline_quotation_mark_match:
+        return inline_quotation_mark_match.group()
 
     quotation_mark_match = quotation_mark_re.match(paragraph)
     if quotation_mark_match:
@@ -16,6 +21,39 @@ def get_first_sentence(paragraph):
 
     first_sentence_match = first_sentence_re.match(paragraph)
     if not first_sentence_match:
-        return None
+        return paragraph
 
     return first_sentence_match.group()
+
+
+def get_sentences(paragraph):
+    """
+    Return a list of sentences from a paragraph
+    """
+    sentences = []
+    while True:
+        sentence = get_first_sentence(paragraph)
+        if sentence == '':
+            break
+        else:
+            sentences.append(sentence)
+
+        # make paragraph smalller
+        paragraph = paragraph.replace(sentence, '', 1)
+
+    return sentences
+
+
+def reduce_sentences(sentences, reduce_words):
+    """
+    if a reduce_word is included in a sentence,
+    the sentence will be removed from sentences
+    """
+    reduce_regexes = [re.compile(word) for word in reduce_words]
+    for regex in reduce_regexes:
+        for sentence in sentences:
+            if regex.search(sentence):
+                sentences.remove(sentence)
+                break
+
+    return sentences
--- sum.py
+++ sum.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
-from .rules import get_first_sentence
-from .utils import unescape
+from .rules import get_first_sentence, reduce_sentences
+from html import unescape
 
 
 def sum(paper):
     paper = unescape(paper)
-    paragraphs = paper.splitlines()
+    paragraphs = [
+        paragraph for paragraph in paper.splitlines() if paragraph != ''
+    ]
 
-    summary = ''
+    sentences_summary = []
+    # Summarizing
     for paragraph in paragraphs:
-        summary += get_first_sentence(paragraph)
+        sentence = ''
+        sentence += get_first_sentence(paragraph)
+        sentences_summary.append(sentence)
+
+    # Remove irrelevant sentences
+    sentences_summary = reduce_sentences(
+        sentences_summary,
+        ['例えば']
+        )
+
+    # Concatenating paragraphs
+    summary = ''
+    for sentence in sentences_summary:
+        summary += sentence + '\n'
 
     return summary
